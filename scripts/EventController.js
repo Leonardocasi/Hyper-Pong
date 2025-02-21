6eea4afe8c5204098b52e993ac0e6e6e6814adde
Leonardocasi <leocasillasg@gmail.com>
2025-02-20 21:47:17 -0600

    Implementación de los modulos

---
+++ scripts/EventController.js
@@ -0,0 +1,11 @@
+import * as System from './main.js'
+
+function start() {
+
+}
+
+function update() {
+
+}
+
+export {start, update}
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,17 @@
-function main() {
-	const canvas = document.getElementById("game")
-	const ctx = canvas.getContext("2d")
+import * as EventController from './EventController.js'
 
-	canvas.width = 1280
-	canvas.height = 720
+const canvas = document.getElementById("game")
+const ctx = canvas.getContext("2d")
+
+canvas.width = 1280
+canvas.height = 720
+
+const perfectFrames = 1000/60
+let DeltaTime = 0
+let lastTimeStamp = 0
+
+function main() {
+	requestAnimationFrame(EventController.update)
 }
+
+export { main, canvas, ctx, DeltaTime }

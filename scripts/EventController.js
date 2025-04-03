c83270119a3c908e54648e62717774027ebfdf3a
Leonardocasi <leocasillasg@gmail.com>
2025-04-03 00:05:06 -0600

    Continuando con las pruebas 2

--- scripts/EventController.js
+++ scripts/EventController.js
@@ -37,7 +37,7 @@ let scoreboard2
 let closestX
 let closestY
 
-const goals = { player1: 0, player2: 0 }
+const goals = { player1: 1, player2: 1 }
 
 
 
@@ -63,8 +63,6 @@ function start() {
 	// Pruebas
 	bombs = [ new Bomb({ x: 100, y: 100 }, 45) ]
 	bombs[0].start()
-
-	console.log(Balls[0].spriteSize)
 }
 
 
@@ -72,7 +70,7 @@ function start() {
 // Función búcle donde se llevará a cabo toda la lógica del jusego.
 // (Se ejecuta en cada frame del juego)
 function update() {
-	// Detección de la pausa.
+	// Detección de la tecla pausa.
 	if ((System.Key.Esc || System.Key.Enter) && !PauseKey) {
 		PauseKey = 1
 
@@ -96,10 +94,31 @@ function update() {
 		player1.update(System.Key.Player1Up, System.Key.Player1Down, System.Key.Player1Power)
 		player2.update(System.Key.Player2Up, System.Key.Player2Down, System.Key.Player2Power)
 	}
-	
-	
 
-	
+
+
+
+	// Funciones de Gameplay que no tienen que ver con la pelota xd
+	if (GameMode == 2) {
+		// Activación de poderes.
+		//	Jugador 1
+		if (System.Key.Player1Serve && scoreboard1.power != -1 && !controlServe1) {
+			controlServe1 = true
+			/*if ( scoreboard1.power == 0 )*/ powerUpMultipleBalls(1)
+			scoreboard1.power = -1
+		}
+				
+		//	Jugador 2
+		if (System.Key.Player2Serve && scoreboard2.power != -1 && !controlServe2) {
+			controlServe2 = true
+			powerUpMultipleBalls(2)
+			scoreboard2.power = -1
+		}
+	}
+
+
+
+
 	// Evaluaciones para todas las bolas dentro del juego
 	Balls.map((Ball, index) => {
 		// Modo Saque del jugador 1
@@ -139,31 +158,16 @@ function update() {
 
 
 
-		// Revisión de colisiones para todas las pelotas del juego.
+		// Actualizaciones durante gameplay
 		if (GameMode == 2) {
-			// Activación de poderes.
-			//	Jugador 1
-			if (System.Key.Player1Serve && scoreboard1.power != -1 && !controlServe1) {
-				controlServe1 = true
-				/*if ( scoreboard1.power == 0 )*/ powerUpMultipleBalls(1)
-				scoreboard1.power = -1
-			}
-
-			//	Jugador 2
-			if (System.Key.Player2Serve && scoreboard2.power != -1 && !controlServe2) {
-				controlServe2 = true
-				powerUpMultipleBalls(2)
-				scoreboard2.power = -1
-			}
-
-
+			// Revisión de colisiones para todas las pelotas del juego.
 			// Verificación de colisiones con los jugadores.
-			if (ballColition(Ball, player1, Ball.position.x, Ball.position.y)) {
+			if (playerColition(Ball, player1)) {
 				newBallAngle(Ball, player1, closestX, closestY)
 				Ball.PlayerColition = true
 			}
 
-			if (ballColition(Ball, player2, Ball.position.x, Ball.position.y)) {
+			if (playerColition(Ball, player2)) {
 				newBallAngle(Ball, player2, closestX, closestY)
 				Ball.PlayerColition = true
 			}
@@ -200,25 +204,11 @@ function update() {
 
 
 			// Verificación de colisiones con el escenario.
-			// Colisión Superior
-			if (Math.round(Ball.position.y) <= Math.round(Ball.radius + scene.density)) {
-				Ball.position.y = Ball.radius + scene.density
-				Ball.speed.y *= -1
-				Ball.getNewAngle()
-				audio.play(audio.sceneHit)
-			}
-
-			// Colisión inferior
-			if (Math.round(System.unscaledHeight - Ball.position.y) <= Math.round(Ball.radius + scene.density)) {
-				Ball.position.y = System.unscaledHeight - Ball.radius - scene.density
-				Ball.speed.y *= -1
-				Ball.getNewAngle()
-				audio.play(audio.sceneHit)
-			}
+			sceneColition(Ball)
 
 
 
-			// Detección de puntos extras. En caso de haber más de una pelota en juego
+			// Detección de puntos extras. En caso de haber más de una pelota en juego (sigue en proceso)
 			if (Balls.length > 1) {
 				// Colision lateral derecha (punto del Jugador 1)
 				if (Math.floor(Ball.position.x) > System.unscaledWidth + Ball.radius + 200) {
@@ -237,8 +227,30 @@ function update() {
 		}
 	})
 	
-	if (GameMode == 2) bombs[0].update()
 
+	// Comportamiento de las bombas del PowerUp de las bombas xd.
+	bombs.map((Bomb, index) => {
+		if (playerColition(Bomb, player1)) {
+			if (goals.player1 > 0) {
+				goals.player1--
+				scoreboard1.update(goals.player1)
+			}
+			bombs.splice(index, 1)
+		}
+
+		if (playerColition(Bomb, player2)) {
+			if (goals.player2 > 0) {
+				goals.player2--
+				scoreboard2.update(goals.player2)
+			}
+			bombs.splice(index, 1)
+		}
+
+		if (GameMode != 3) {
+			sceneColition(Bomb)
+			Bomb.update()
+		}
+	})
 
 
 	// Las siguientes líneas de código solo pueden suceder con una sola pelota en juego.
@@ -287,18 +299,49 @@ function update() {
 		Ball.draw()
 	})
 
-	bombs[0].draw()
+	bombs.map(Bomb => {
+		Bomb.draw()
+	})
+}
+
+
+
+
+// ==========================================================
+// ==== Funciones extra para el funcionamiento del juego ====
+// ==========================================================
+
+
+
+
+// Función para revisar las colisiones con el escenario
+function sceneColition(Ball) {
+	// Colisión Superior
+	if (Math.round(Ball.position.y) <= Math.round(Ball.radius + scene.density)) {
+		Ball.position.y = Ball.radius + scene.density
+		Ball.speed.y *= -1
+		Ball.getNewAngle()
+		audio.play(audio.sceneHit)
+	}
+
+	// Colisión inferior
+	if (Math.round(System.unscaledHeight - Ball.position.y) <= Math.round(Ball.radius + scene.density)) {
+		Ball.position.y = System.unscaledHeight - Ball.radius - scene.density
+		Ball.speed.y *= -1
+		Ball.getNewAngle()
+		audio.play(audio.sceneHit)
+	}
 }
 
 
 
 // Función para la detección de colisiones de las pelotas con los jugadores.
-function ballColition(Ball, player, BallPosX, BallPosY) {
-	closestX = Math.max(player.position.x, Math.min(BallPosX, player.position.x + Players.width))
-	closestY = Math.max(player.position.y, Math.min(BallPosY, player.position.y + Players.height))
+function playerColition(Ball, player) {
+	closestX = Math.max(player.position.x, Math.min(Ball.position.x, player.position.x + Players.width))
+	closestY = Math.max(player.position.y, Math.min(Ball.position.y, player.position.y + Players.height))
 	
-	let distanceX = BallPosX - closestX
-	let distanceY = BallPosY - closestY
+	let distanceX = Ball.position.x - closestX
+	let distanceY = Ball.position.y - closestY
 	
 	let distance = Math.sqrt(distanceX ** 2 + distanceY ** 2)
 
@@ -343,7 +386,8 @@ function newBallAngle(Ball, player, closestX, closestY) {
 
 // Función para el poder de las multiples pelotas.
 function powerUpMultipleBalls(player) {
-	let BallIndex = myMath.random(0, Balls.length - 1)
+	// Esto me ayudará a seleccionar una bola aleatoria en caso de ya haber varias.
+	let BallIndex = myMath.random(0, Balls.length -1)
 	let Length = Balls.length
 
 	for (let i = 0; i < 10; i++) {
@@ -364,7 +408,10 @@ function powerUpMultipleBalls(player) {
 
 
 
-// 
+// Función para el poder de la bola bomba.
+function powerUpBombBall(player) {
+	
+}
 
 
 
